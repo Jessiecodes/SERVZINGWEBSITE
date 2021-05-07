c6d83eed5fd215a125446e0d770709ac8b1dc860
Jessica Leverett <Jessak1202@gmail.com>
2021-05-07 14:14:43 -0500

    next edits

---
+++ src/components/pages/Services/Data.js
@@ -0,0 +1,97 @@
+export const serviceObjOne = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Exclusive Access',
+    headline: 'We pride ourselves on Superior customer service',
+    description:
+      'SERVZING is your one-call solution for a wide range of home maintenance and repair needs. Our techs are fully insured and experienced. ',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: 'https://cdn.pixabay.com/photo/2018/03/10/12/00/paper-3213924_960_720.jpg',
+    alt: 'Credit Card'
+  };
+  
+  export const serviceObjTwo = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Security Surveillance',
+    headline: 'Alarm Systems ',
+    description:
+      'The Honeywell LYNX Touch Panels are some of the most popular alarm systems offered from Honeywell. These are wireless all-in-one systems that each feature their own touchscreen controller. They are capable of performing almost any application needed from a modern system.',
+    buttonLabel: 'Learn More',
+    imgStart: 'start',
+    img: 'images/lynx5000.jpg',
+    alt: 'security cameras in miami florida'
+  };
+  
+  export const serviceObjThree = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'High Quality Security Systems',
+    headline: 'Security Surveillance',
+    description:
+      "Protect what matters most with this industry-leading 4K IP security camera system. Featuring a 16-channel NVR (NVR) and eight 4K Varifocal Nocturnal security cameras, this system is perfect for any home or business.  ",
+    buttonLabel: 'Start Now',
+    imgStart: 'true',
+    img: 'images/16chann_8cam.png',
+    alt: 'Vault'
+  };
+  
+  export const serviceObjFour = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Great Prices for a Great Service',
+    headline: 'HandyMan Services',
+    description:
+      'Regardless of the size of your home, maintaining it can be a struggle. While you may not have the time or the tools to accomplish everything you want to, what you do have is a local SERVZING with a reliable team of experts who offer handyman services.',
+    buttonLabel: '',
+    imgStart: 'start',
+    img: 'images/handyman.jpg',
+    alt: 'Vault'
+  };
+
+  export const serviceObjRing = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Keeping things neat and tidy',
+    headline: 'Home Improvement',
+    description:' By power washing your deck, siding, driveway, and other exterior areas around your home, you can quickly and effectively remove built-up dirt, sediment, and other debris. This not only helps the areas look beautiful, but it can also reduce wear and tear by getting rid of mold, algae, chalk, insects, weeds, and more. Such build-up will, over time, break down and cause damage to wood / masonry and degrade paint / stains. ',
+    buttonLabel: '',
+    imgStart: 'false',
+    img: 'images/suburb.svg',
+    alt: 'Vault'
+  };
+
+  export const serviceObjWeb = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Market your Brand',
+    headline: 'Digital Services',
+    description:'Web design, High Fidelity Prototyping, UI/ UX Design, Graphic, brand & Logo design, video and photo editing. ',
+    buttonLabel: '',
+    imgStart: 'start',
+    img: 'images/svg-16.svg',
+    alt: 'Vault'
+  };
+
+
+
+  export const serviceObjFive = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'More Service, More Security ',
+    headline: 'Remote Viewing',
+    description:
+      'Enjoy security even more, with no monthly contracts and financing available. Watch your properties while your away with the FREE app included with your installation.',
+    buttonLabel: 'Sign Up Now',
+    imgStart: 'start',
+    img: 'images/LorexHomeApp.png',
+    alt: 'Vault'
+  };
--- src/components/pages/Services/Services.js
+++ src/components/pages/Services/Services.js
@@ -1,11 +1,131 @@
-import React from 'react'
+import React from 'react';
+import './Services.css';
+import HeroSection from '../../HeroSection';
+import { serviceObjOne } from './Data';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Fade from 'react-reveal/Fade';
+import CardGroup from 'react-bootstrap/CardGroup'
+import Card from 'react-bootstrap/Card';
+import {Tabs, Tab} from 'react-bootstrap-tabs';
+import { AiFillVideoCamera } from "react-icons/ai";
+import { GrMonitor } from "react-icons/gr";
+import { GiRingingAlarm, GiDrill } from "react-icons/gi";
+
 
 function Services() {
-    return (
+  return (
+    <>
+    <Fade>
+      <HeroSection {...serviceObjOne} />
+    </Fade>
+
+    <Container>
+    <h1> Let the security experts take care over you and your loved ones.</h1>
+    </Container>
+    <div className="Service_top_img">
         <div>
-            <h1> SERVICES </h1>
+        <img className="img-50" src="https://images.unsplash.com/photo-1612173562294-8a977ef9d895?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="security cameras near me" />
         </div>
-    )
+        <div>
+        <img className="img-50" src="https://images.unsplash.com/photo-1489710437720-ebb67ec84dd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="security cameras near me" />
+        </div>
+    </div>
+
+<Container>
+  <Row>
+    <Col xs={12}>
+        <CardGroup>
+        <Card>
+        <div className="icon_container">
+            <AiFillVideoCamera /> 
+        </div>
+        <Card.Body>
+        <Card.Title>Security Cameras</Card.Title>
+        <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in to
+            additional content. This content is a little bit longer.
+        </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+        <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+     </Card>
+    <Card>
+    <div className="icon_container">
+        <GiRingingAlarm />
+    </div>
+    <Card.Body>
+      <Card.Title>Alarm Monitoring</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <div className="icon_container">
+        <GrMonitor />
+    </div>
+    <Card.Body>
+      <Card.Title>TV. Mounting </Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+  <div className="icon_container">
+    <GiDrill />
+  </div>
+    <Card.Body>
+      <Card.Title>HandyMan Services</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+
+</CardGroup>
+
+
+    </Col>
+    <Col lg={12} className="QUOTE">REACH OUT TODAY AND GET A PERSONALIZED QUOTE <a href="tel:7862873326"> 786-287-3326 </a></Col>
+  </Row>
+
+  <Row>
+  <Tabs defaultActiveKey="home" transition={false} id="noanim-tab-example">
+  <Tab eventKey="home" title="Home">
+
+  </Tab>
+  <Tab eventKey="profile" title="Profile">
+
+  </Tab>
+  <Tab eventKey="contact" title="Contact" disabled>
+ 
+  </Tab>
+</Tabs>
+
+
+
+  </Row>
+</Container>
+    </>
+  );
 }
 
-export default Services
+export default Services;
